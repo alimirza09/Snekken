e1d7a825ba7612960f983b7eb5aa71cbfc433a0c
alimirza09 <AliMirza@skiff.com>
2024-04-11 12:38:11 +0500

    idk

--- src/main.c
+++ src/main.c
@@ -12,10 +12,11 @@ int main(void)
 
   InitWindow(screenWidth, screenHeight, "Snekken");
   ChangeDirectory("..");
-  Vector2 ballPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
-  Image background = LoadImage("assets/background.png");
-  Texture2D texture = LoadTextureFromImage(background); // Image converted to texture, uploaded to GPU memory (VRAM)
-  UnloadImage(background);                              // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+  Vector2 rectanglePosition = {(float)screenWidth / 2, (float)screenHeight / 2};
+  Image background = LoadImage("assets/Enter-name.png");
+  ImageResizeNN(&background, 800, 150);
+  Texture2D backgroundTexture = LoadTextureFromImage(background); // Image converted to texture, uploaded to GPU memory (VRAM)
+  UnloadImage(background);                                        // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
 
   //--------------------------------------------------------------------------------------
   int FPS;
@@ -24,32 +25,39 @@ int main(void)
   {
     // Update
     //----------------------------------------------------------------------------------
+    struct Rectangle testRec = {50, 50, rectanglePosition.x, rectanglePosition.y};
+    struct Rectangle backgroundCollider = {0, 300, backgroundTexture.width, backgroundTexture.height};
     float dt = GetFrameTime();
     if (IsKeyDown(KEY_D))
-      ballPosition.x += 200.0f * dt;
+      rectanglePosition.x += 100.0f * dt;
     if (IsKeyDown(KEY_A))
-      ballPosition.x -= 200.0f * dt;
+      rectanglePosition.x -= 100.0f * dt;
     if (IsKeyDown(KEY_W))
-      ballPosition.y -= 200.0f * dt;
-    if (IsKeyDown(KEY_S))
-      ballPosition.y += 200.0f * dt;
-    printf("(%f, %f)", ballPosition.x, ballPosition.y);
-
+      rectanglePosition.y -= 95.0f * dt;
+    if (IsKeyDown(KEY_S) && !CheckCollisionRecs(backgroundCollider, testRec))
+      rectanglePosition.y += 100.0f * dt;
+    testRec.x = rectanglePosition.x;
+    testRec.y = rectanglePosition.y;
+    if (!CheckCollisionRecs(backgroundCollider, testRec))
+    {
+      rectanglePosition.y += 5;
+    }
+    printf("Rectangle (%f, %f)", rectanglePosition.x, rectanglePosition.y);
+    printf("Background(%d, %d)", backgroundTexture.height, backgroundTexture.width);
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawTexture(texture, 750, 350, WHITE);
-
+    DrawTexture(backgroundTexture, 0, 300, WHITE);
     DrawFPS(10, 10);
-    DrawCircleV(ballPosition, 50, MAROON);
+    DrawRectangle(rectanglePosition.x, rectanglePosition.y, 50, 50, GOLD);
     EndDrawing();
     //----------------------------------------------------------------------------------
   }
 
   // De-Initialization
   //-------------------------------------------------------------------------------------
-  UnloadTexture(texture);
+  UnloadTexture(backgroundTexture);
   CloseWindow(); // Close window and OpenGL context
   //--------------------------------------------------------------------------------------
 
