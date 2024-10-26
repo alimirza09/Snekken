803096e2ca079e4b9ee273643c39fde96468bbd9
Ali Mirza <106017617+alimirza09@users.noreply.github.com>
2024-10-26 21:56:10 +0500

    Delete build directory

--- build/.project.gf
+++
@@ -1,4 +0,0 @@
-[executable]
-path=/home/ali/Projects/Snekken/build/Snekken
-arguments=-o Snekken src/main.c src/snake.c -I src -L /usr/local/lib -lraylib -lm -ldl -lpthread -lrt
-ask_directory=1
--- build/CMakeFiles/Snekken.dir/src/snake.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/Snekken.dir/src/snake.c.o: \
- /home/ali/Projects/Snekken/src/snake.c /usr/include/stdc-predef.h \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdbool.h \
- /home/ali/Projects/Snekken/src/snake.h
