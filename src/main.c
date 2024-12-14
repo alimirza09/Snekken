f7616bf6e27bc471d51b250e6f05a416427fa5d6
alimirza09 <AliMirza@skiff.com>
2024-12-14 14:08:48 +0500

    removed old sprites

--- src/main.c
+++ src/main.c
@@ -2,9 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define SNAKE_LENGTH 256
-#define SQUARE_SIZE 31
-#define SPRITE_SIZE 36*4 
+#define SPRITE_SIZE 36 * 4
 
 typedef struct Snake {
   Vector2 position;
@@ -50,11 +48,11 @@ bool IsCooldownReady(int cooldownTimer, int requiredTime);
 
 void DrawPlayerAnimation(Player *player, int frameRow, bool flip);
 
-
-void UpdatePlayerAnimation(Player *player, int maxFrames, int frameRow, bool flip);
-void HandlePlayerAnimation(Player *player, Player *opponent, int animationType, int maxFrames,
-                           int cooldown, int hpReduction,int xAccel, int yAccel,
-                           int frameRow, bool flip);
+void UpdatePlayerAnimation(Player *player, int maxFrames, int frameRow,
+                           bool flip);
+void HandlePlayerAnimation(Player *player, Player *opponent, int animationType,
+                           int maxFrames, int cooldown, int hpReduction,
+                           int xAccel, int yAccel, int frameRow, bool flip);
 
 Texture2D SafeLoadTexture(const char *filename);
 const int screenWidth = 800;
@@ -87,9 +85,16 @@ int main() {
   Rectangle player1Collider = {screenWidth / 2.0f - SPRITE_SIZE,
                                backgroundCollider.y - SPRITE_SIZE - 2,
                                SPRITE_SIZE - 48, SPRITE_SIZE};
-  Player player1 = {
-      0,   0,0, 100, player1Texture, player1Collider, {0.0f, 0.0f}, {0.0f, 0.0f},
-      100, true};
+  Player player1 = {0,
+                    0,
+                    0,
+                    100,
+                    player1Texture,
+                    player1Collider,
+                    {0.0f, 0.0f},
+                    {0.0f, 0.0f},
+                    100,
+                    true};
 
   //----------------------------------------------------------------------------------
   // Player 2
@@ -98,13 +103,19 @@ int main() {
   Rectangle player2Collider = {screenWidth / 2.0f + 25,
                                backgroundCollider.y - SPRITE_SIZE - 2,
                                SPRITE_SIZE - 36, SPRITE_SIZE};
-  Player player2 = {
-      0,   0,0, 100, player2Texture, player2Collider, {0.0f, 0.0f}, {0.0f, 0.0f},
-      100, true};
+  Player player2 = {0,
+                    0,
+                    0,
+                    100,
+                    player2Texture,
+                    player2Collider,
+                    {0.0f, 0.0f},
+                    {0.0f, 0.0f},
+                    100,
+                    true};
 
   // Debug mode and snake mode
   bool debugMode = false;
-  bool snakeRunning = false;
 
   const float gravitationalForce = 4;
 
@@ -140,9 +151,9 @@ int main() {
     //----------------------------------------------------------------------------------
     // Player 2
     //----------------------------------------------------------------------------------
-    player2.acceleration =
-        keyDetection(KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_K, KEY_L,
-                     player2.acceleration, &player2.isGrounded, &player2.animation);
+    player2.acceleration = keyDetection(
+        KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_K, KEY_L, player2.acceleration,
+        &player2.isGrounded, &player2.animation);
 
     if (IsKeyPressed(KEY_P)) {
       debugMode = !debugMode;
@@ -167,97 +178,95 @@ int main() {
     //----------------------------------------------------------------------------------
     BeginDrawing();
 
-    if (!snakeRunning) {
-      if (player2.hp <= 0) {
-        DrawText("PLAYER 1 WINS", 28, screenHeight / 2.5, 90, RED);
-      } else if (player1.hp <= 0) {
-        DrawText("PLAYER 2 WINS", 28, screenHeight / 2.5, 90, RED);
-      } else {
+    if (player2.hp <= 0) {
+      DrawText("PLAYER 1 WINS", 28, screenHeight / 2.5, 90, RED);
+    } else if (player1.hp <= 0) {
+      DrawText("PLAYER 2 WINS", 28, screenHeight / 2.5, 90, RED);
+    } else {
 
-        ClearBackground(BLACK);
-        DrawRectangleGradientV(0, 0, screenWidth, screenHeight,
-                               (Color){77, 180, 227, 100},
-                               (Color){68, 121, 227, 100});
-        DrawTexture(backgroundTexture, 0, screenHeight - 30, WHITE);
+      ClearBackground(BLACK);
+      DrawRectangleGradientV(0, 0, screenWidth, screenHeight,
+                             (Color){77, 180, 227, 100},
+                             (Color){68, 121, 227, 100});
+      DrawTexture(backgroundTexture, 0, screenHeight - 30, WHITE);
 
-        if (debugMode) {
-          DrawFPS(10, 10);
-          printf("player1.collider(%f, %f) \n", player1.collider.x,
-                 player1.collider.y);
-          printf("player2.collider(%f, %f) \n", player2.collider.x,
-                 player2.collider.y);
-          printf("player1.velocity(%f, %f) \n", player1.velocity.x,
-                 player1.velocity.y);
-          printf("player2.velocity(%f, %f) \n", player2.velocity.x,
-                 player2.velocity.y);
-          printf("player1.acceleration(%f, %f) \n", player1.acceleration.x,
-                 player1.acceleration.y);
-          printf("player1.acceleration(%f, %f) \n", player1.acceleration.x,
-                 player1.acceleration.y);
-          printf("player1.animation: %d \n", player1.animation);
-          printf("player2.animation: %d \n", player2.animation);
-          printf("player1.frameCounter: %d \n", player1.frameCounter);
-          printf("player2.frameCounter: %d \n", player2.frameCounter);
-          printf("dt: %f \n", dt);
-          printf("player1.hp: %d \n", player1.hp);
-          printf("player2.hp: %d \n", player2.hp);
-          printf("player1.isGrounded: %d \n", player1.isGrounded);
-        }
+      if (debugMode) {
+        DrawFPS(10, 10);
+        printf("player1.collider(%f, %f) \n", player1.collider.x,
+               player1.collider.y);
+        printf("player2.collider(%f, %f) \n", player2.collider.x,
+               player2.collider.y);
+        printf("player1.velocity(%f, %f) \n", player1.velocity.x,
+               player1.velocity.y);
+        printf("player2.velocity(%f, %f) \n", player2.velocity.x,
+               player2.velocity.y);
+        printf("player1.acceleration(%f, %f) \n", player1.acceleration.x,
+               player1.acceleration.y);
+        printf("player1.acceleration(%f, %f) \n", player1.acceleration.x,
+               player1.acceleration.y);
+        printf("player1.animation: %d \n", player1.animation);
+        printf("player2.animation: %d \n", player2.animation);
+        printf("player1.frameCounter: %d \n", player1.frameCounter);
+        printf("player2.frameCounter: %d \n", player2.frameCounter);
+        printf("dt: %f \n", dt);
+        printf("player1.hp: %d \n", player1.hp);
+        printf("player2.hp: %d \n", player2.hp);
+        printf("player1.isGrounded: %d \n", player1.isGrounded);
+      }
 
-    switch (player1.animation) {
-        case 1:
-            HandlePlayerAnimation(&player1, &player2, 1, 11, 20, 0, 100, -90, 0, false);
-            break;
-        case 2:
-            HandlePlayerAnimation(&player1, &player2, 2, 7, 50, 20, 150, -90, 1, false);
-            break;
-          case 3:
-            HandlePlayerAnimation(&player1, &player2, 3, 7,
-                                  0, 0,0, 0,
-                                  2, false);
-          break;
-        default:
-            DrawPlayerAnimation(&player1, 0, false);
-            break;
-    }
+      switch (player1.animation) {
+      case 1:
+        HandlePlayerAnimation(&player1, &player2, 1, 11, 20, 0, 100, -90, 0,
+                              false);
+        break;
+      case 2:
+        HandlePlayerAnimation(&player1, &player2, 2, 7, 50, 20, 150, -90, 1,
+                              false);
+        break;
+      case 3:
+        HandlePlayerAnimation(&player1, &player2, 3, 7, 0, 0, 0, 0, 2, false);
+        break;
+      default:
+        DrawPlayerAnimation(&player1, 0, false);
+        break;
+      }
 
-    switch (player2.animation) {
-        case 1:
-            HandlePlayerAnimation(&player2, &player1, 1, 11, 20, 20, -100, -30, 0, true);
-            break;
-        case 2:
-            HandlePlayerAnimation(&player2, &player1, 2, 7, 50, 30, -150, -90, 1, true);
-            break;
-          case 3:
-            HandlePlayerAnimation(&player2, &player1, 3, 7,
-                                  0, 0,0, 0,
-                                  2, true);
-          break;
-        default:
-            DrawPlayerAnimation(&player2, 0, true);
-            break;
-    }
-        
-        // HP Draw
-        DrawRectangleLines(50, 50, 81, 11, GRAY);
-        DrawRectangleLines(screenWidth - 50 - 80, 50, 100 / 1.22, 11, GRAY);
-        DrawRectangleGradientH(50, 50, player1.hp / 1.249, 10,
-                               (Color){255, 51, 65, 100}, RED);
-        DrawRectangleGradientH(screenWidth - 50 - 80, 50, player2.hp / 1.249,
-                               10, (Color){255, 51, 65, 100}, RED);
+      switch (player2.animation) {
+      case 1:
+        HandlePlayerAnimation(&player2, &player1, 1, 11, 20, 20, -100, -30, 0,
+                              true);
+        break;
+      case 2:
+        HandlePlayerAnimation(&player2, &player1, 2, 7, 50, 30, -150, -90, 1,
+                              true);
+        break;
+      case 3:
+        HandlePlayerAnimation(&player2, &player1, 3, 7, 0, 0, 0, 0, 2, true);
+        break;
+      default:
+        DrawPlayerAnimation(&player2, 0, true);
+        break;
+      }
 
-        // Player Identifier draw
-        DrawText("PLAYER 1", 50, 40, 5, RAYWHITE);
-        DrawText("PLAYER 2", screenWidth - 50 - 80, 40, 5, RAYWHITE);
-        // Colliders draw
-        if (debugMode) {
-          DrawRectangleLines(player1.collider.x, player1.collider.y,
-                             player1.collider.width, player1.collider.height,
-                             RAYWHITE);
-          DrawRectangleLines(player2.collider.x, player2.collider.y,
-                             player2.collider.width, player2.collider.height,
-                             RAYWHITE);
-        }
+      // HP Draw
+      DrawRectangleLines(50, 50, 81, 11, GRAY);
+      DrawRectangleLines(screenWidth - 50 - 80, 50, 100 / 1.22, 11, GRAY);
+      DrawRectangleGradientH(50, 50, player1.hp / 1.249, 10,
+                             (Color){255, 51, 65, 100}, RED);
+      DrawRectangleGradientH(screenWidth - 50 - 80, 50, player2.hp / 1.249, 10,
+                             (Color){255, 51, 65, 100}, RED);
+
+      // Player Identifier draw
+      DrawText("PLAYER 1", 50, 40, 5, RAYWHITE);
+      DrawText("PLAYER 2", screenWidth - 50 - 80, 40, 5, RAYWHITE);
+      // Colliders draw
+      if (debugMode) {
+        DrawRectangleLines(player1.collider.x, player1.collider.y,
+                           player1.collider.width, player1.collider.height,
+                           RAYWHITE);
+        DrawRectangleLines(player2.collider.x, player2.collider.y,
+                           player2.collider.width, player2.collider.height,
+                           RAYWHITE);
       }
     }
 
@@ -340,45 +349,43 @@ Vector2 UpdatePosition(Vector2 accelerationVector, Vector2 velocityVector,
 
 // Function to check cooldown
 bool IsCooldownReady(int cooldownTimer, int requiredTime) {
-    return cooldownTimer >= requiredTime;
+  return cooldownTimer >= requiredTime;
 }
 
 // Function to draw player animations
 void DrawPlayerAnimation(Player *player, int frameRow, bool flip) {
-    int flipModifier = flip ? -1 : 1;
-    DrawTexturePro(player->texture,
-                   (Rectangle){SPRITE_SIZE * player->frameCounter,
-                               frameRow * SPRITE_SIZE,
-                               flipModifier * SPRITE_SIZE, SPRITE_SIZE},
-                   (Rectangle){player->collider.x + (flip ? -48 : 0),
-                               player->collider.y, SPRITE_SIZE, SPRITE_SIZE},
-                   (Vector2){0, 0}, 0.0f, WHITE);
+  int flipModifier = flip ? -1 : 1;
+  DrawTexturePro(player->texture,
+                 (Rectangle){SPRITE_SIZE * player->frameCounter,
+                             frameRow * SPRITE_SIZE, flipModifier * SPRITE_SIZE,
+                             SPRITE_SIZE},
+                 (Rectangle){player->collider.x + (flip ? -48 : 0),
+                             player->collider.y, SPRITE_SIZE, SPRITE_SIZE},
+                 (Vector2){0, 0}, 0.0f, WHITE);
 }
 
-void UpdatePlayerAnimation(Player *player, int maxFrames, int frameRow, bool flip) {
+void UpdatePlayerAnimation(Player *player, int maxFrames, int frameRow,
+                           bool flip) {
   if (player->animationTimer > 3 && frameRow != 2) {
     player->frameCounter++;
     player->animationTimer = 0;
-  }
-  else if(player->animationTimer > 6 && frameRow == 2){
-    if(player->frameCounter < 3 || player->frameCounter >= 5){
+  } else if (player->animationTimer > 6 && frameRow == 2) {
+    if (player->frameCounter < 3 || player->frameCounter >= 5) {
       player->frameCounter++;
       player->animationTimer = 0;
-    }
-    else if((player -> frameCounter <= 6 && player->frameCounter > 3) && player->isGrounded){
+    } else if ((player->frameCounter <= 6 && player->frameCounter > 3) &&
+               player->isGrounded) {
       player->frameCounter++;
       player->animationTimer = 0;
-    }
-    else if(player->frameCounter == 3){
-      player->acceleration.y-=100.0f;
-      player->isGrounded=false;
+    } else if (player->frameCounter == 3) {
+      player->acceleration.y -= 100.0f;
+      player->isGrounded = false;
       player->frameCounter++;
     }
-  }
-  else {
+  } else {
     player->animationTimer++;
   }
-    if (player->frameCounter < maxFrames) {
+  if (player->frameCounter < maxFrames) {
     int flipModifier = flip ? -1 : 1;
     DrawTexturePro(player->texture,
                    (Rectangle){SPRITE_SIZE * player->frameCounter,
@@ -387,40 +394,40 @@ void UpdatePlayerAnimation(Player *player, int maxFrames, int frameRow, bool fli
                    (Rectangle){player->collider.x + (flip ? -48 : 0),
                                player->collider.y, SPRITE_SIZE, SPRITE_SIZE},
                    (Vector2){0, 0}, 0.0f, WHITE);
-    } else {
-        player->animation = 0;
-        player->frameCounter = 0;
-        player->cooldownTimer = 0;
-    }
+  } else {
+    player->animation = 0;
+    player->frameCounter = 0;
+    player->cooldownTimer = 0;
   }
-void HandlePlayerAnimation(Player *player, Player *opponent, int animationType, int maxFrames,
-                           int cooldown, int hpReduction, int xAccel, int yAccel,
-                           int frameRow, bool flip) {
-    if (IsCooldownReady(player->cooldownTimer, cooldown)) {
-        UpdatePlayerAnimation(player, maxFrames, frameRow, flip);
+}
+void HandlePlayerAnimation(Player *player, Player *opponent, int animationType,
+                           int maxFrames, int cooldown, int hpReduction,
+                           int xAccel, int yAccel, int frameRow, bool flip) {
+  if (IsCooldownReady(player->cooldownTimer, cooldown)) {
+    UpdatePlayerAnimation(player, maxFrames, frameRow, flip);
 
-        if (player->animation == 0) { // Animation just ended
-            if ((player->collider.x - opponent->collider.x) > -100 &&
-                (player->collider.x - opponent->collider.x) < 100) {
-                opponent->hp -= hpReduction;
-                opponent->acceleration.x += xAccel;
-                opponent->acceleration.y += yAccel;
-            }
-        }
-    } else {
-        player->animation = 0;
+    if (player->animation == 0) { // Animation just ended
+      if ((player->collider.x - opponent->collider.x) > -100 &&
+          (player->collider.x - opponent->collider.x) < 100) {
+        opponent->hp -= hpReduction;
+        opponent->acceleration.x += xAccel;
+        opponent->acceleration.y += yAccel;
+      }
     }
+  } else {
+    player->animation = 0;
+  }
 }
 Texture2D SafeLoadTexture(const char *filename) {
-    if (FileExists(filename)) {
-        Image image = LoadImage(filename);
-        ImageResizeNN(&image, SPRITE_SIZE * 12, SPRITE_SIZE * 3);
-        if (image.data != NULL) {
-            Texture2D texture = LoadTextureFromImage(image);
-            UnloadImage(image);
-            return texture;
-        }
+  if (FileExists(filename)) {
+    Image image = LoadImage(filename);
+    ImageResizeNN(&image, SPRITE_SIZE * 12, SPRITE_SIZE * 3);
+    if (image.data != NULL) {
+      Texture2D texture = LoadTextureFromImage(image);
+      UnloadImage(image);
+      return texture;
     }
-    TraceLog(LOG_ERROR, "Failed to load texture: %s", filename);
-    return (Texture2D){0}; // Return a null/invalid texture
+  }
+  TraceLog(LOG_ERROR, "Failed to load texture: %s", filename);
+  return (Texture2D){0}; // Return a null/invalid texture
 }
